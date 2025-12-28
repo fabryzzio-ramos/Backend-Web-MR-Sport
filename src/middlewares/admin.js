768a31ff4968ae2916df843958976e2c9441e83e
fabryzzio-ramos <fabryzzio@gnail.com>
2025-12-27 23:56:33 -0500

    admin

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -1,5 +1,4 @@
 const admin = (req, res, next) => {
-    if (!req.usuario) return res.status(401).json({ mensaje: "No autenticado" });
     if (req.usuario.rol !== "admin") return res.status(403).json({ mensaje: "Acceso denegado (solo admin)" });
 
     next();
